5e230d9a6633d22ab73826e3c0f2dce883ba1eee
Yonkeu Steve <71908316+yokwejuste@users.noreply.github.com>
2022-06-10 18:22:05 +0100

    modifications (#3)
    * feat: updates
    
    * Update settings.py

--- library/settings.py
+++ library/settings.py
@@ -17,49 +17,22 @@ import dj_database_url
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
-django_on_heroku.settings(locals())
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-%4%)y$)b3$%e-ck-ek8emcc*^2go-0fn#1k83xd9w+_ufg199q'
-
-# SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
-
-ALLOWED_HOSTS = [
-    'https://library237.herokuapp.com',
-    'https://www.library237.herokuapp.com',
-    'library237.herokuapp.com',
-    '127.0.0.1',
-    'localhost',
-]
-
-
-# Application definition
-
-INSTALLED_APPS = [
-    'django.contrib.admin',
-    'django.contrib.auth',
-    'django.contrib.contenttypes',
-    'django.contrib.sessions',
-    'django.contrib.messages',
-    'django.contrib.staticfiles',
-    'books',
-    'django_on_heroku',
-]
-
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'whitenoise.middleware.WhiteNoiseMiddleware',
 ]
+
+django_on_heroku.settings(locals())
 
 ROOT_URLCONF = 'library.urls'
 
