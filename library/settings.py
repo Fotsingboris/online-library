f52def40cc007d43efcd0380931f7ed549a50110
Fotsingboris <fotsingtchoupe1@gmail.com>
2022-06-10 15:23:24 +0100

    feat: add more requirements

--- library/settings.py
+++ library/settings.py
@@ -12,10 +12,12 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 
 import os
 from pathlib import Path
+import django_on_heroku
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
-
+django_on_heroku.settings(locals())
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
