f091e61a94cef38ffe48ce84d4c57ee0e87f0e17
Fotsingboris <fotsingtchoupe1@gmail.com>
2022-06-20 01:30:23 +0100

    feractor: modified the models.py

--- books/models.py
+++ books/models.py
@@ -5,7 +5,6 @@ from django.db import models
 
 class Store(models.Model):
     name = models.CharField(max_length=100)
-    addresse = models.CharField(max_length=100)
     name_book = models.CharField(max_length=100)
     author = models.CharField(max_length=100)
     cover_picture_book = models.ImageField(upload_to='images/')
